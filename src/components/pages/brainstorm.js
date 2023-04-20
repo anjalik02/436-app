a9b9859e633a21b08547f9fb5c67b0b872028be6
Anjali Kumar <anjalik02@tamu.edu>
2023-04-20 01:01:09 -0500

    sticky note frontend

--- src/components/pages/brainstorm.js
+++ src/components/pages/brainstorm.js
@@ -3,9 +3,24 @@ import React from 'react';
 const Brainstorm = () => {
   return (
     <div className='App'>
-      <h1 style={{ color: '#3500D3', fontSize: '4em' }}>
-        Sticky Notes
-      </h1>
+      <div class="sticky-note">
+        <div class="header">
+          <span class="title">Song 1</span>
+          <span class="close-btn">&times;</span>
+        </div>
+        <div class="content">
+          <p>Song ideas: 120 bpm happy song</p>
+        </div>
+      </div>
+      <div class="sticky-note">
+        <div class="header">
+          <span class="title">Song 2</span>
+          <span class="close-btn">&times;</span>
+        </div>
+        <div class="content">
+          <p>Song ideas: none yet</p>
+        </div>
+      </div>
     </div>
   );
 };
