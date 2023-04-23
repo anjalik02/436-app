990c8672a3cfad8c8384155219b8b95b5b3506cd
“hdhulipala02” <hdhulipala02@tamu.edu>
2023-04-22 23:38:18 -0500

    Revert "magic link working without route blocking or styling"
    This reverts commit 596a9c2c6c3d0040a0b978ccc879202bbd0b46a3.

--- src/App.js
+++ src/App.js
@@ -1,85 +1,29 @@
-// import React, { useState } from 'react';
-// import './App.css';
-// import Navbar from './components/navbar';
-// import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
-// import Home from './components/pages/home';
-// import Brainstorm from './components/pages/brainstorm';
-// import Generate from './components/pages/generate';
-// import Notate from './components/pages/notate';
-// import Check from './components/pages/check';
-// import Share from './components/pages/share';
-// import LoginHome from './components/pages/loginHome';
-// import LoginLink from './components/pages/loginLink';
-
-// function App() {
-//   const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-//   const handleLogin = () => {
-//     setIsLoggedIn(true);
-//   };
-
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path='/login' element={<LoginHome handleLogin={handleLogin} />} />
-//         <Route path='/login-link' element={<LoginLink />} />
-//         {isLoggedIn ? (
-//           <>
-//             <Navbar isLoggedIn={isLoggedIn} />
-//             <Route exact path='/' element={<Home />} />
-//             <Route path='/home' element={<Home />} />
-//             <Route path='/brainstorm' element={<Brainstorm />} />
-//             <Route path='/generate' element={<Generate />} />
-//             <Route path='/notate' element={<Notate />} />
-//             <Route path='/check' element={<Check />} />
-//             <Route path='/share' element={<Share />} />
-//           </>
-//         ) : (
-//           <Navigate to='/login' />
-//         )}
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
-
-
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Navbar from './components/navbar';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './components/pages/home';
 import Brainstorm from './components/pages/brainstorm';
 import Generate from './components/pages/generate';
 import Notate from './components/pages/notate';
 import Check from './components/pages/check';
 import Share from './components/pages/share';
-import LoginHome from './components/pages/loginHome';
-import LoginLink from './components/pages/loginLink';
-
+  
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const handleLogin = () => {
-    setIsLoggedIn(true);
-  };
-
-  return (
+return (
     <Router>
-      <Routes>
-        <Route path='/login' element={<LoginHome />} />
-        <Route path='/login-link' element={<LoginLink />} />
+    <Navbar />
+    <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/home' element={<Home />} />
-        <Route path='/brainstorm' element={<Brainstorm />} />
-        <Route path='/generate' element={<Generate />} />
-        <Route path='/notate' element={<Notate />} />
-        <Route path='/check' element={<Check />} />
-        <Route path='/share' element={<Share />} />
-      </Routes>
+        <Route path='/home' element={<Home/>} />
+        <Route path='/brainstorm' element={<Brainstorm/>} />
+        <Route path='/generate' element={<Generate/>} />
+        <Route path='/notate' element={<Notate/>} />
+        <Route path='/check' element={<Check/>} />
+        <Route path='/share' element={<Share/>} />
+    </Routes>
     </Router>
-  );
+);
 }
-
+  
 export default App;
