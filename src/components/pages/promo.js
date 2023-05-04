3e898aa65668764b0b77b082facaf84149e24ce0
kirthivel-ramesh <kirthivel.ramesh@tamu.edu>
2023-05-04 11:36:49 -0500

    final

--- src/components/pages/promo.js
+++ src/components/pages/promo.js
@@ -2,20 +2,19 @@ import React,{Component} from 'react';
 
 class Promo extends Component {
  render() {
- 
-   return (
-      <div className="App">
-        <div className="box">
-          <div style={{ color: 'white', fontSize: '4em' }}>
-            Promo Video
-          </div>
-          <p style={{ color: 'white', fontSize: '1.5em' }}>
-          Insert video here
-          </p>
-        </div>
+  return (
+    <div className="App">
+      <div className="box">
+        {/* <h1 style={{ color: 'white', fontSize: '4em' }}>
+          Presentation Slides
+        </h1> */}
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/oyWZHxj7hys" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
       </div>
-    );
+    </div>
+  );
  }
 }
+
+
 
 export default Promo;
